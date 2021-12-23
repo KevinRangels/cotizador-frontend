2c6307e7aad1f8355a7b2dd849b3a795cec57504
Kevin Rangel <krangel107@gmail.com>
2021-12-23 17:12:14 -0500

    fix footer text

--- src/components/UI/Footer.js
+++ src/components/UI/Footer.js
@@ -9,7 +9,7 @@ export const Footer = () => {
           <div className="col-lg-6 mb-lg-0 mb-4">
             <div className="copyright text-center text-sm text-muted text-lg-start">
               <div className="copyright text-center text-sm text-muted text-lg-start">
-                © Todos los derechos reservados, 2022, <strong>Softla ERP</strong>
+                © Todos los derechos reservados 2022 - <strong>Softla ERP</strong>
               </div>
             </div>
           </div>
